0a2fa2dfcb6f004fdc734a2d886ed3ecafe4b83f
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-11-19 14:14:36 +0100

    fix: search root markers upwards

--- lua/overseer-extra/utils.lua
+++ lua/overseer-extra/utils.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 M.is_cmake_project = function(cwd)
-	return #vim.fs.find("CMakeLists.txt", { path = cwd, type = "file" }) > 0
+	return #vim.fs.find("CMakeLists.txt", { path = cwd, upward = true, type = "file" }) > 0
 end
 
 M.is_python_project = function(cwd)
-	return #vim.fs.find("pyproject.toml", { path = cwd, type = "file" }) > 0
+	return #vim.fs.find("pyproject.toml", { path = cwd, upward = true, type = "file" }) > 0
 end
 
 return M
